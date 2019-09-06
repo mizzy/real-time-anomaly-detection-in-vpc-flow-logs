642ce1f47e1ea347ca058912ceb68f3bc55b0724
Gosuke Miyashita <gosukenator@gmail.com>
2019-09-06 14:13:52 +0900

    Add a js file for lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,15 @@
 resource "aws_lambda_function" "kinesis_analytics_process_compressed_record" {
   function_name = "KinesisAnalyticsProcessCompressedRecord"
+  filename         = data.archive_file.kinesis_analytics_process_compressed_record.output_path
   handler       = "index.handler"
 
   runtime = "nodejs10.x"
   role    = aws_iam_role.kinesis_analytics_process_compressed_record.arn
   timeout = 60
+}
+
+data "archive_file" "kinesis_analytics_process_compressed_record" {
+  type        = "zip"
+  source_dir  = "lambda/KinesisAnalyticsProcessCompressedRecord"
+  output_path = "lambda/upload/KinesisAnalyticsProcessCompressedRecord.zip"
 }
